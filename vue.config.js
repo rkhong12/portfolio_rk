d81430aeb5dd063f446b8ef6c1614f43acfe17fa
홍려경 <dhnp3879@gmail.com>
2026-03-17 15:21:02 +0900

    Fix: config.js

--- vue.config.js
+++ vue.config.js
@@ -2,27 +2,22 @@
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // })
-module.exports = {    
-  publicPath: process.env.NODE_ENV === 'production'
-   ? '/rkhong/'
-   : '/',
-  outputDir: 'docs',
-  css : {
-    loaderOptions : {
-      scss: { 
+module.exports = {
+  publicPath: "./",
+  outputDir: "dist",
+  css: {
+    loaderOptions: {
+      scss: {
         additionalData: ` 
           @import "@/assets/scss/variables.scss"; 
-        `, 
+        `,
       },
-    }
+    },
   },
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-          args[0].title = "Hongryeokyeong's Portfolio";
-          return args;
-      })
-  }
- } 
- 
+  // chainWebpack: (config) => {
+  //   config.plugin("html").tap((args) => {
+  //     args[0].title = "Hongryeokyeong's Portfolio";
+  //     return args;
+  //   });
+  // },
+};
